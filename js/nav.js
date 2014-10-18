d0a8f53efb6a43b32249a8be4031175f7ce72530
Channing Pear <PearSquirrel@users.noreply.github.com>
2014-10-17 21:22:53 -0500

    made smaller class activate when not mobile

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,23 @@
+function getWidth() {
+  if (self.innerHeight) {
+    return self.innerWidth;
+  }
+
+  if (document.documentElement && document.documentElement.clientHeight) {
+    return document.documentElement.clientWidth;
+  }
+
+  if (document.body) {
+    return document.body.clientWidth;
+  }
+}
+
 function init() {
     window.addEventListener('scroll', function(e){
         var distanceY = window.pageYOffset || document.documentElement.scrollTop,
             shrinkOn = 100,
             header = document.querySelector(".site-nav");
-        if (distanceY > shrinkOn) {
+        if (distanceY > shrinkOn && getWidth() > 500) {
             classie.add(header,"smaller");
         } else {
             if (classie.has(header,"smaller")) {
@@ -12,6 +26,8 @@ function init() {
         }
     });
 }
+
+
 
 function togglePullDown() {
     id = document.querySelector(".site-nav");
